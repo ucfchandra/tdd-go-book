294b7f4421ced8a9aa997242673a8a63ffc9c210
Chandra <chandra.kethireddy@ucf.edu>
2024-11-16 22:51:41 -0500

    add TestMain

--- chapter02/calculator/engine_test.go
+++ chapter02/calculator/engine_test.go
@@ -1,9 +1,30 @@
 package calculator_test
 
 import (
+	"os"
+	"log"
 	"testing"
 	"github.com/ucfchandra/tdd-go-book/chapter02/calculator"
 )
+
+func TestMain(m *testing.M) {
+	// setup statements
+	setup()
+	// run the tests
+	e := m.Run()
+	// cleanup statements
+	teardown()
+	// report the exit code
+	os.Exit(e)
+}
+
+func setup() {
+	log.Println("Setting up")
+}
+
+func teardown() {
+	log.Println("Tearing down")
+}
 
 func TestAdd(t *testing.T) {
 	// Arrange
